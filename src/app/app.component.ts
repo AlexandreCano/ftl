e926ba027cb9e3e72af9846f7dfe3c95cf00ec78
AlexandreCano <alexanndre.cano@gmail.com>
2018-01-24 09:49:48 +0100

    assets

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,37 +25,38 @@ export class AppComponent {
         });
     }
     preload() {
-        this.game.time.advancedTiming = true;
-        this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-        this.game.scale.pageAlignHorizontally = true;
-        this.game.scale.pageAlignVertically = true;
+        // this.game.time.advancedTiming = true;
+        // this.game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        // this.game.scale.pageAlignHorizontally = true;
+        // this.game.scale.pageAlignVertically = true;
+        // this.game.scale.refresh();
         this.game.load.tilemap('spaceship', 'assets/Spaceship/Spaceship.json', null, Phaser.Tilemap.TILED_JSON);
         this.game.load.image('tiles', 'assets/Spaceship/Spaceship.png');
-        this.game.load.spritesheet('perso', 'assets/ss.png', 64, 64, 273);
+        this.game.load.spritesheet('perso', 'assets/image/RPGSoldier32x32.png', 32, 35, 45);
     }
     create() {
         this.game.stage.backgroundColor = '#787878';
         const map = this.game.add.tilemap('spaceship');
         map.addTilesetImage('Spaceship 3', 'tiles');
+
         const floor = map.createLayer('Floor', 1536, 1536);
-        floor.resizeWorld();
+        // floor.resizeWorld();
         const blocked = map.createLayer('Blocked', 1536, 1536);
-        blocked.resizeWorld();
+        // blocked.resizeWorld();
         const doorActivators = map.createLayer('Door_Activators', 1536, 1536);
-        doorActivators.resizeWorld();
+        // doorActivators.resizeWorld();
         const doors = map.createLayer('Doors', 1536, 1536);
-        doors.resizeWorld();
+        // doors.resizeWorld();
         const objects = map.createLayer('Objects', 1536, 1536);
-        objects.resizeWorld();
+        // objects.resizeWorld();
         const use = map.createLayer('Use_Markers', 1536, 1536);
-        use.resizeWorld();
+        // use.resizeWorld();
         const walls = map.createLayer('Walls', 1536, 1536);
-        walls.resizeWorld();
+        // walls.resizeWorld();
+
         this.perso = this.game.add.sprite(6 * 64, 8 * 64, 'perso');
-
-        this.perso.animations.add('walk_right', [143, 144, 145, 146, 147, 148, 149, 150, 151], 50, true, true);
-
-        this.perso.animations.play('walk_right', 50, true);
+        this.perso.animations.add('walk_right', [9, 10, 11, 12], 30, true, true);
+        this.perso.animations.play('walk_right', 30, true);
     }
     update() {
         this.time = new Date();
